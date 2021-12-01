99e1ed98779e99b9f249e9358e97fcab93edfc50
Egbert Teeselink <egbert@talkjs.com>
2021-12-01 08:35:00 +0000

    initial commit

---
+++ Kiosk.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Kiosk.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var i = 5;
+        Assert.Equal(i, 6);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/eteeselink/ooti2021_left)
+
