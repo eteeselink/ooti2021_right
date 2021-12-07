e040fe3b432ff10a5524371ac16a19a822fae1a1
apooyan <pooyan.adibi@gmail.com>
2021-12-07 12:55:34 +0000

    added CreateHtmlContent

--- Kiosk.App/Report.cs
+++ Kiosk.App/Report.cs
@@ -8,5 +8,41 @@ namespace Kiosk.App;
 public class Report {
     public void Run() {
         Console.WriteLine("Report");
+        
     }
+}
+
+private string CreateHtmlContent(){
+        string htmlContent = @"<html>
+        <body>
+        <div>
+        <ul>
+<li>
+<span>Pizza</span>
+<span>12</span>
+</li>
+<li>
+<span>Hambuger</span>
+<span>6</span>
+</li>
+<li>
+<span>Spagetti</span>
+<span>11</span>
+</li>
+<li>
+<span>Iranian Food</span>
+<span>30</span>
+</li>
+<li>
+<span>Dutch Food</span>
+<span>Are you kidding me?</span>
+</li>
+<li>
+<span></span>
+<span></span>
+</li>
+        </ul>
+        </div>
+        </body>
+        </<html>";
 }
