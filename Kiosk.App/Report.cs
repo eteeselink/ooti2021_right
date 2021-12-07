f8416e637286a5d180a3f9119160b8bf2a476738
Georgios Evangelou <george_evangelou@windowslive.com>
2021-12-07 12:52:50 +0000

    Added fuction to create HTML file

--- Kiosk.App/Report.cs
+++ Kiosk.App/Report.cs
@@ -5,8 +5,42 @@ using System.Linq;
 
 namespace Kiosk.App;
 
-public class Report {
-    public void Run() {
+public class Report
+{
+    public void Run()
+    {
         Console.WriteLine("Report");
+
+
+        WriteToFile("This is a test");
+    }
+
+
+
+
+    private void WriteToFile(String htmlText)
+    {
+        var rootDir = AppContext.BaseDirectory + "/../../../../";
+
+        string fileName = rootDir + "report.html";
+
+        try
+        {
+            // Check if file already exists. If yes, delete it.     
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            // Create a new file     
+            using (StreamWriter sw = File.CreateText(fileName))
+            {
+                sw.WriteLine(htmlText);
+            }
+        }
+        catch (Exception Ex)
+        {
+            Console.WriteLine(Ex.ToString());
+        }
     }
 }
