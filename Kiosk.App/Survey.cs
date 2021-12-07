9a890c7b462767c85c58719ccb92395f2da4bf09
jachian <jachian@gmail.com>
2021-12-07 14:42:38 +0000

    james update

--- Kiosk.App/Survey.cs
+++ Kiosk.App/Survey.cs
@@ -6,10 +6,51 @@ using System.Linq;
 namespace Kiosk.App;
 
 class Survey {
+
+    private String question = "dummy question?...";
+    private List<string> options;
+    private int[] responseCount = {0,0};
+
+
+
     public void Run() {
-        Console.WriteLine("Survey: getting questionaire from read module");
-        Console.WriteLine("Survey: asking user 1");
-        Console.WriteLine("Survey: asking user 2");
-        Console.WriteLine("Survey: asking user 3");
+
+        // get the question /////////////////////////
+        String answer = "";
+        Read read = new Read();
+        Question q = read.GetQuestion();
+        question = q.question;
+        this.options = q.options;
+
+
+        Ask ask = new Ask();
+        ask.setQuestion(q);
+
+
+        for(int i = 0; i < 5; i++)
+        {
+            ask.Run();
+            answer = ask.getAnswer();
+
+            Console.WriteLine(answer);
+
+        }
+
+
+
+
+
+        
+        //Console.WriteLine("Survey: getting questionaire from read module");
+        //Console.WriteLine("Survey: asking user 1");
+        //Console.WriteLine("Survey: asking user 2");
+        //Console.WriteLine("Survey: asking user 3");
+    }
+
+
+
+    private void getQuestions()
+    {
+
     }
 }
