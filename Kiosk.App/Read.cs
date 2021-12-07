5d49049c99e00aec1d51b9182f90bacf0b9fadb2
Egbert Teeselink <egbert@talkjs.com>
2021-12-07 11:32:17 +0000

    Fix bug in sample code

--- Kiosk.App/Read.cs
+++ Kiosk.App/Read.cs
@@ -14,8 +14,8 @@ class Read {
     // this is just an example of how to read a file,
     // modify/delete as you see fit.
     public string ReadQuestionsFile() {
-        // we run in <root>/KioskApp/bin/Debug/net6.0, so gotta go up 4 levels
-        var rootDir = "../../../../";
+        // we run in <root>/Kiosk.App/bin/Debug/net6.0, so gotta go up 4 levels
+        var rootDir = AppContext.BaseDirectory + "/../../../../";
         
         return File.ReadAllText(rootDir + "questions.txt");
     }
