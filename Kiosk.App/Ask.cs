c84fe272e73928dd26d4cbc6bef0b07d8ced91cd
Renisa Suryahadikusumah <renisas@ymail.com>
2021-12-07 13:16:45 +0000

    stupid phase of Ask

--- Kiosk.App/Ask.cs
+++ Kiosk.App/Ask.cs
@@ -5,8 +5,46 @@ using System.Linq;
 
 namespace Kiosk.App;
 
+
 class Ask {
+    List<string> questions = new List<string>();
+    List<List<string>> options = new List<List<string>>();
+    List<string> answers = new List<string>();
+    int questionIndex;
+
     public void Run() {
-        Console.WriteLine("Ask");
+        //here will collect questions and answers from survey class
+        questions =  new List<string>(){"What food do you want to choose?", "Do you like cat or dog?"};
+        options.Add(new List<string>() {"pizza", "lahmachun", "bitterballen"});
+        options.Add(new List<string>() {"cat", "dog"});
+        answers = new List<string>();
+        questionIndex = 0;
+        nextQuestion();
+    }
+
+    public void sendAnswers(){
+        //send answers to the survey class
+    }
+
+    public void nextQuestion(){
+        string question = questions[questionIndex];
+        List<string> option = options[questionIndex];
+        string answer;
+        Console.WriteLine(question);
+        int i = 1;
+        foreach(string o in option){
+            Console.WriteLine(i + ". " + o);
+            i += 1;
+        }
+        Console.WriteLine("Your vote : ");
+        answer = Console.ReadLine();
+        answers.Add(answer);
+        questionIndex += 1;
+        if(questionIndex == questions.Count){
+            Console.WriteLine("Thanks!");
+            sendAnswers();
+        }else{
+            nextQuestion();
+        }
     }
 }
